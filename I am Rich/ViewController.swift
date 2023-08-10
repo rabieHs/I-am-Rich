db739c7e104f8bfd234d3bb7b76ceee527c5dccc
rabieHs <rabiehoussaini@gmail.com>
2023-08-10 15:43:05 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by rabie houssaini on 10/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
